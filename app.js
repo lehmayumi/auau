e0b13872b633e52a2b23270198c3e1f918a62605
lehmayumi <leticiakayo30@gmail.com>
2024-10-24 10:59:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
   var map = L.map('map').setView([-23.52772015578366, -46.66783567505396], 13);
 
-
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -19,12 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
  
   // Create a marker with popup and add it to the map
   var marker = L.marker([-23.52772015578366, -46.66783567505396]).addTo(map);
-  marker.bindPopup("namoradinhos !").openPopup();
+  marker.bindPopup("Amada turma de DMD, você está aqui, onde há muitas possibilidades de crescimento !").openPopup();
  
  
   
     // Create a marker with popup and add it to the map
   var marker2 = L.marker([-23.52772015578366, -46.66783567505396], {icon: greenIcon}).addTo(map);
-  marker2.bindPopup("namoradinhos !").openPopup();
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
  
 });
