5d679940f52b6c7af44fe0b966b91a84b76b54e6
lehmayumi <leticiakayo30@gmail.com>
2024-10-24 10:42:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
   var greenIcon = L.icon({
-    iconUrl: 'https://pt.pngtree.com/freepng/pomeranian-dog-ai-generated_13162300.html',
+    iconUrl: 'https://static.vecteezy.com/system/resources/thumbnails/016/761/881/small_2x/the-dog-smiles-because-he-is-happy-png.png',
  
     iconSize:     [80, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
-var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
+var marker2 = L.marker([-23.52772015578366, -46.66783567505396], {icon: greenIcon}).addTo(map);
   marker2.bindPopup("auauau").openPopup();
