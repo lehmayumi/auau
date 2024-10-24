a6cf6558a2802e75f06f110fd9fe5964344ddd2b
lehmayumi <leticiakayo30@gmail.com>
2024-10-24 10:50:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,27 @@ document.addEventListener('DOMContentLoaded', function () {
   var map = L.map('map').setView([-23.52772015578366, -46.66783567505396], 13);
 
 
-  // Add a tile layer to the map from OpenStreetMap
+ // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-
-
-  // Create a marker with popup and add it to the map
-  var marker = L.marker([-23.52772015578366, -46.66783567505396]).addTo(map);
-  marker.bindPopup("dia especial!").openPopup();
-});
-
+ 
   var greenIcon = L.icon({
-    iconUrl: 'https://png.pngtree.com/png-clipart/20230514/original/pngtree-smile-dog-on-white-background-png-image_9160783.png',
+    iconUrl: 'https://artpoin.com/wp-content/uploads/2023/07/artpoin-coracoes-fundo-transparente-png2.png',
  
     iconSize:     [80, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-
-var marker2 = L.marker([-23.52772015578366, -46.66783567505396], {icon: greenIcon}).addTo(map);
-  marker2.bindPopup("auauau").openPopup();
+ 
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.52772015578366, -46.66783567505396]).addTo(map);
+  marker.bindPopup("Amada turma de DMD, você está aqui, onde há muitas possibilidades de crescimento !").openPopup();
+ 
+ 
+  
+    // Create a marker with popup and add it to the map
+  var marker2 = L.marker([-23.52772015578366, -46.66783567505396], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+ 
+});
